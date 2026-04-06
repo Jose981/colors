5761a865da735c23d6c0cb1781f70c51d1f0dcc1
Pepe MR <pepemartos98@gmail.com>
2026-04-06 12:50:52 +0200

    entrega ejercicio colores

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,61 @@
-function boxColor ({color, value}) {
+import { useRef, useEffect } from "react";
+
+function BoxColor({ color, value }) {
+  const boxRef = useRef();
+
+  // español → inglés
+  const colorMap = {
+    rojo: "red",
+    verde: "green",
+    rosa: "pink",
+    amarillo: "yellow",
+    morado: "purple",
+    blanco: "white",
+    azul: "blue",
+    aqua: "aqua",
+    oliva: "olive",
+  };
+
+  // inglés → español
+  const reverseMap = {
+    red: "rojo",
+    green: "verde",
+    pink: "rosa",
+    yellow: "amarillo",
+    purple: "morado",
+    white: "blanco",
+    blue: "azul",
+    aqua: "aqua",
+    olive: "oliva",
+  };
+
+  useEffect(() => {
+    const box = boxRef.current;
+    if (!box) return;
+
+    const input = value.trim().toLowerCase();
+    const translated = colorMap[input] || input;
+
+    if (translated === color) {
+      box.style.backgroundColor = color;
+
+      // 🔥 mostramos en español
+      box.textContent = `¡Soy ${reverseMap[color]}!`;
+    } else {
+      box.style.backgroundColor = color;
+      box.textContent = `No soy ${value}`;
+    }
+  }, [value, color]);
+
   return (
-    <>
-    </> 
-  )
+    <div
+      ref={boxRef}
+      className="box"
+      style={{ backgroundColor: color}}
+    >
+      No soy ${reverseMap[color]}
+    </div>
+  );
 }
-export default boxColor;  
+
+export default BoxColor;
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,39 @@
-
+import { useState } from "react";
+import BoxColor from "./BoxColor";
 
 function MyForm() {
+  const [value, setValue] = useState("");
+
+  const colors = [
+    "red",
+    "green",
+    "pink",
+    "yellow",
+    "purple",
+    "white",
+    "blue",
+    "aqua",
+    "olive",
+  ];
 
   return (
-    <></>
+    <>
+      <div>
+        <h2>Buscador de colores</h2>
+        <input
+          type="text"
+          placeholder="Añade un color..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+
+      <div className="container">
+        {colors.map((color) => (
+          <BoxColor key={color} color={color} value={value} />
+        ))}
+      </div>
+    </>
   );
 }
 
